ee3414ed12a5fb518a9d621931e7b9d75ab0ef9f
Ma57eR <ggeorgiev83@gmail.com>
2019-04-08 18:01:45 +0300

    Second lesson

---
+++ Java/src/main/OperExpre.java
@@ -0,0 +1,96 @@
+package main;
+
+public class OperExpre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Оператори
+		// Arithhmetic
+		// Logical
+		// Bitwise
+		// Comparison
+		// Assignment
+		// Operators
+		
+		// Unary - -17
+		// Binary - 2 операнди - 3+5
+		// Ternary Operator - Тернарен е с 3 операнди
+		
+//		int sum = 2 * 3 + 5;
+//		System.out.println(sum);
+
+		/* Видове оператори
+		 * 	Arithmetic: + - '*' / % ++ -- 
+		 *  Logic: && || ^ ! : И ИЛИ Отричане
+		 *  Binary: & ^ ~ << >> | : И Или Ксор Отричане 
+		 *  Comparison: == != <> <= >=
+		 *  Assignment: = += -+ '*=' /= %= &= ^= <<= >>= |=
+		 *  String Concatenation: +
+		 *  Other: . [] () ? : new
+		 */
+		
+//		int num = 10;
+//		if (num % 2 == 1) {
+//			System.out.println("Number is Odd");
+//		}
+//		else {
+//				System.out.println("Number is Even");
+//			}
+//		System.out.println(num % 2 == 1);
+		
+//		int num = 15;
+//		System.out.println(num++); // Нум е = на 15 и след като принти увеличава с 1
+//		System.out.println(num); //Изкарва новата стойност на Нум
+//		System.out.println(++num); // Първо добавя 1 към Нум и след това изкарва резултата с новата стойност
+//		System.out.println(num / 3); // Когато делим, винаги ни показва остатък цялото число. Ако искаме да има символи след десетичната запетая, използва се х.0
+		
+		// Logical Operators
+		
+		// || OR
+		// true || false
+		
+//		boolean bool = false || false;
+//		System.out.println(bool);
+		
+		// && AND
+		// true && true
+		
+//		boolean bool = true && true;
+//		System.out.println(bool);
+		
+		// ^ XOR
+		// Дали са различни стойностите - връща true при различни
+		
+//		boolean bool = true ^ false;
+//		System.out.println(bool);
+		
+		// Bitwise
+		
+//		int first = 0b0101;
+//		int second = 0b0011;
+//		
+//		System.out.println(~second);
+		
+//		int a = 1;
+//		String b = "1";
+//		System.out.println(a = b);
+//		
+//		int second = 0b0011;
+//		System.out.println(second >> 1);
+//		System.out.println(second >> 2);
+		
+//		int num = 4;
+//		num >>= 5;
+//		System.out.println(num);
+		
+		int i = 5;
+		long l = 1;
+		System.out.println(l);
+		
+		long ll = 5555555555555555L;
+		int ii = (int) ll;
+		System.out.println(ii + "\n" + ll);
+		
+	}
+
+}
